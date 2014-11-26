a29122a0181dc7bddf9a4e3c5dd9c02cb69c4c07
Paul Ecoffet <ecoffet.paul@gmail.com>
2014-11-26 15:48:23 +0100

    Small docstring corrections

--- coins.py
+++ coins.py
@@ -9,23 +9,23 @@ class Coins(Counter):
 
     Usage:
     ```
-    > coins = Coins({200: 0, 100: 2, 50: 4, 20: 3, 10: 1})
-    > coins.value
+    >>> coins = Coins({200: 0, 100: 2, 50: 4, 20: 3, 10: 1})
+    >>> coins.value
     470
 
-    > coins2 = Coins({200:0, 100:0, 50:0, 20:2, 10:0})
-    > coins + coins2
+    >>> coins2 = Coins({200:0, 100:0, 50:0, 20:2, 10:0})
+    >>> coins + coins2
     Coins({200:0, 100:2, 50:4, 20:5, 10:1})
 
-    > coins - coins2
+    >>> coins - coins2
     Coins({200:0, 100:2, 50:4, 20:1, 10:1})
     ```
     Warning: Negative coins amount is not forbidden (yet)
 
     In place addition and subtraction are also allowed
     ```
-    > coins.add(coins2)
-    > coins
+    >>> coins.add(coins2)
+    >>> coins
     Coins({200:0, 100:2, 50:4, 20:5, 10:1})
 
     ```
@@ -37,11 +37,11 @@ class Coins(Counter):
 
         Usage:
         ```
-        > Coins({200:2, 100:5})
+        >>> Coins({200:2, 100:5})
         Coins({200:2, 100:5})
-        > Coins([200, 200, 100, 100, 100, 100, 100])
+        >>> Coins([200, 200, 100, 100, 100, 100, 100])
         Coins({200:2, 100:5})
-        > Coins()
+        >>> Coins()
         Coins()
         ```
         """
@@ -69,7 +69,7 @@ class Coins(Counter):
 
         How to use:
 
-        > coins.Compute_change(200)
+        >>> coins.Compute_change(200)
         Coins({200:0, 100:1, 50:2, 20:0, 10:0})
         """
         temp = copy.copy(self)
@@ -122,8 +122,8 @@ class Coins(Counter):
 
         Usage:
         ```
-        > c = Coins({100: 4, 50: 1})
-        > c.value
+        >>> c = Coins({100: 4, 50: 1})
+        >>> c.value
         450
         ```
         """
@@ -132,5 +132,5 @@ class Coins(Counter):
             total += value * amount
         return total
 
-    add = Counter.update  # Update for addition is not clear, addition of
-                          # add for symetry with subtract
+    add = Counter.update  # Update for addition is not clear, the alias
+                          # add is more coherent with subtract
