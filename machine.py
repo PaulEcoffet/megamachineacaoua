e4a581ace7413b39015ce1ce58c60144eff2590a
Paul Ecoffet <ecoffet.paul@gmail.com>
2014-11-26 15:48:42 +0100

    few fixes and MachineMaintenance created

--- machine.py
+++ machine.py
@@ -57,12 +57,14 @@ class Machine(object):
                 pass
             else:
                 if type_ == 'sugar':
-                    if isinstance(new_val,list):
+                    if isinstance(new_val, list) and len(new_val) == 4:
+                        valid = True
                         for i in range(0,3):
                             if not 0 <= new_val[i] <= new_val[i+1]:
+                                valid = False
                                 break
-                            elif i == 2:
-                                self.stock_prices['sugar'] = new_val
+                        if valid:
+                            self.stock_prices['sugar'] = new_val
                     else:
                         raise TypeError('Sugar must be a list of 4 digits')
 
@@ -244,6 +246,64 @@ class MachineFunc(object):
     def factory(self):
         return self.m
 
+    def __repr__(self):
+        return 'Machine à café en fonctionnement'
+
+
+class MachineMaintenance(object):
+
+    def __init__(self, machine_factory):
+        self.m = machine_factory
+
+    def edit_prices(self, **prices):
+        self.m.edit_prices(**prices)
+
+    def edit_stocks(self, **stocks):
+        self.m.edit_stock(**stocks)
+
+    def refill_stocks(self):
+        self.m.refill_stocks()
+
+    def edit_coins(self, coins):
+        self.m.edit_coins()
+
+    def refill_coins(self):
+        self.m.refill_coins()
+
+    @property
+    def max_stocks(self):
+        return self.m.max_stocks
+
+    @property
+    def max_coins(self):
+        return self.m.max_coins
+
+    @property
+    def stocks(self):
+        return self.m.stocks
+
+    @property
+    def coins(self):
+        return self.m.coins
+
+    @property
+    def log(self):
+        return self.m.log
+
+    @property
+    def pretty_log(self):
+        return self.m.pretty_log
+
+    @property
+    def stock_prices(self):
+        return self.m.stock_prices
+
+    @property
+    def coins(self):
+        return self.m.coins
+
+    def __repr__(self):
+        return 'Machine à café en maintenance'
 
 
 # Run the tests on import (as asked)
